3aaec20feb3359c5f3427dd834f3a753acb4037c
kbkozlev <kbkozlev@gmail.com>
2023-04-21 08:10:37 +0300

    updated functions

---
+++ functions.py
@@ -0,0 +1,34 @@
+import PySimpleGUI as sg
+import pandas as pd
+from pathlib import Path
+
+
+def c(elems):
+    return [sg.Stretch(), *elems, sg.Stretch()]
+
+
+def is_valid_path(filepath):
+    if filepath and Path(filepath).exists():
+        return True
+    sg.popup_no_titlebar("File not found")
+    return False
+
+
+def combine_worksheets(excel_file_path):
+    df = pd.concat(pd.read_excel(excel_file_path, sheet_name=None), ignore_index=True)
+    filename = Path(excel_file_path).stem + "_combined"
+    return df, filename
+
+
+def combine_workbooks():
+    pass
+
+
+def convert_to_csv(df, filename, output_folder):
+    outputfile = Path(output_folder) / f"{filename}.csv"
+    df.to_csv(outputfile, index=False)
+
+
+def convert_to_excel(df, filename, output_folder):
+    outputfile = Path(output_folder) / f"{filename}.xlsx"
+    df.to_excel(outputfile, index=False)
